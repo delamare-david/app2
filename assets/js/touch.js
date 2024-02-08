7b6ed19dca23f5a78a0c5fd22f1aeba89b8cef76
delamare-david <david22du76140@gmail.com>
2024-02-08 16:24:23 +0100

    modif 2

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -1,5 +1,5 @@
 var touch = document.getElementById("touch");
-
+var debug = document.getElementById("debug");
 var hammertime = new Hammer(document.body, {});
 var ofLeft = touch.offsetLeft;
 var ofTop = touch.offsetTop;
@@ -9,6 +9,7 @@ hammertime.on('pan pinch', function (event) {
     let distance = event.distance;
     let additionalEvent = event.additionalEvent;
     console.log(additionalEvent, distance);
+    debug.innerText = additionalEvent;
     /* "panright" "panleft" "panup" "pandown" */
     switch (additionalEvent) {
         case "panright":
